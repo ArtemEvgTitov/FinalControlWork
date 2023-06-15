2053978435dd650aee5c22a90854673454f76e21
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-06-15 23:53:38 +0300

    Удалён лишний код

--- Tasks_13-15/Animals/Pack_Animal/PackAnimal.java
+++ Tasks_13-15/Animals/Pack_Animal/PackAnimal.java
@@ -1,23 +1,12 @@
 package Animals.Pack_Animal;
 
 import Animals.Animal;
-import Animals.Pet.Pet;
 
-import java.util.List;
 
 public class PackAnimal extends Animal {
-
-    List<String> commands;
 
     PackAnimal(int id, String name, String age, String voise, String eat ){
         super(id, name, age, voise, eat);
     }
 
-    public void addCommand(String command){
-        this.commands.add(command);
-    }
-
-    public void print(Pet pet){
-        System.out.print(pet.toString() + "\nCommands: " + commands.toString());
-    }
 }
