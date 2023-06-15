97f0940d7d980d260f777cedb9476b11b6d573db
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-06-15 23:54:59 +0300

    Визуальные правки

--- Tasks_13-15/Animals/Pack_Animal/Camel.java
+++ Tasks_13-15/Animals/Pack_Animal/Camel.java
@@ -2,7 +2,7 @@ package Animals.Pack_Animal;
 
 public class Camel extends PackAnimal {
 
-    public Camel(int id, String name, String age){
+    public Camel(int id, String name, String age) {
         super(id, name, age, "Ворчит", "Солянка, полынь, верблюжья колючка, саксаул");
     }
 }
