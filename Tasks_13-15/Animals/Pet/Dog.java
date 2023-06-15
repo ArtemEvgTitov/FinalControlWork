d6e7a84a8da8061645cd901a7a7a77b779c49f05
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-06-15 23:55:42 +0300

    Визуальные правки

--- Tasks_13-15/Animals/Pet/Dog.java
+++ Tasks_13-15/Animals/Pet/Dog.java
@@ -1,7 +1,8 @@
 package Animals.Pet;
 
-public class Dog extends Pet{
-    public Dog(int id, String name, String age){
+public class Dog extends Pet {
+    
+    public Dog(int id, String name, String age) {
         super(id, name, age, "Лает", "Мясо, птица, рыба");
     }
 }
