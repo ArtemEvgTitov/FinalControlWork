fcab6ace4aea3de915bb1e3d383fe4b452e69656
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-06-15 23:58:46 +0300

    Создан Main

---
+++ Tasks_13-15/Main.java
@@ -0,0 +1,8 @@
+import Presenter.Presenter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Presenter app = new Presenter();
+        app.run();
+        }
+    }
