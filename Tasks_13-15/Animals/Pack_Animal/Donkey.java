f786e475320637426302b9357d6a55f821393b82
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-06-15 23:54:41 +0300

    Визуальные правки

--- Tasks_13-15/Animals/Pack_Animal/Donkey.java
+++ Tasks_13-15/Animals/Pack_Animal/Donkey.java
@@ -2,7 +2,7 @@ package Animals.Pack_Animal;
 
 public class Donkey extends PackAnimal {
 
-    public Donkey(int id, String name, String age){
+    public Donkey(int id, String name, String age) {
         super(id, name, age, "Ревёт", "Овес, ячмень, кукуруза, отруби, жмых, сено");
     }
 }
