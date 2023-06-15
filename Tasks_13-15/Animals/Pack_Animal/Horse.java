29d16c7b0d9ce46aa8ce18f1a875e79347c7bb38
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-06-15 23:54:25 +0300

    Визуальные правки

--- Tasks_13-15/Animals/Pack_Animal/Horse.java
+++ Tasks_13-15/Animals/Pack_Animal/Horse.java
@@ -2,7 +2,7 @@ package Animals.Pack_Animal;
 
 public class Horse extends PackAnimal {
 
-    public Horse(int id, String name, String age){
+    public Horse(int id, String name, String age) {
         super(id, name, age, "Ржёт", "Сено, солома, овёс, трава");
     }
 }
