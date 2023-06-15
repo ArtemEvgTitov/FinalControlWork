4da018eea61fa0be3778346b997ae85f71922495
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-06-15 23:54:02 +0300

    Визуальные правки

--- Tasks_13-15/Animals/Pack_Animal/PackAnimal.java
+++ Tasks_13-15/Animals/Pack_Animal/PackAnimal.java
@@ -2,10 +2,9 @@ package Animals.Pack_Animal;
 
 import Animals.Animal;
 
-
 public class PackAnimal extends Animal {
 
-    PackAnimal(int id, String name, String age, String voise, String eat ){
+    PackAnimal(int id, String name, String age, String voise, String eat) {
         super(id, name, age, voise, eat);
     }
 
